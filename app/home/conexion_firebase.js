eff921ea796c7aff2bc722bb998315ebb3573e01
carolina silva <carolinasguzman@gmail.com>
2019-04-14 07:38:45 -0400

    feat: ultimo

---
+++ app/home/conexion_firebase.js
@@ -0,0 +1,37 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAQeRh2IKGHJMlW--scfgycYBQvHKyWjy8",
+  authDomain: "ripleylabs-f9425.firebaseapp.com",
+  databaseURL: "https://ripleylabs-f9425.firebaseio.com",
+  projectId: "ripleylabs-f9425",
+  storageBucket: "ripleylabs-f9425.appspot.com",
+  messagingSenderId: "542394243494"
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+  db.collection("catalog")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            $('#here').append(`<div class="col-xs-12">
+            <h4 class="images-title">${doc.data().name} (${doc.data().co_id})</h4>
+            <a onClick="gethol('${doc.data().co_id}')"><img class="productos" src="${doc.data().img}"></a>
+            </div>`)
+        });
+    })
+    .catch(function(error) {
+      console.log("Error getting documents: ", error);
+  });
+
+  function gethol(co_id) {
+
+    var docData = {
+        query: co_id,
+        timestamp: new Date()
+    };
+
+    db.collection("consultas").doc('lueSbdBvAIvXbBMZtEC4').set(docData).then(function() {
+        console.log("Document successfully written!");
+    }, { merge: true });
+}
