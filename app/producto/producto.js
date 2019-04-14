497195c5dcad33138a981c35fb2b54003f436298
carolina silva <carolinasguzman@gmail.com>
2019-04-14 01:39:15 -0400

    mas

---
+++ app/producto/producto.js
@@ -0,0 +1,10 @@
+function volver() {
+    window.location.href = "../home/home.html"
+}
+
+function llamarVendedor() {
+    var modal = modelContinuar();
+    if (modal==true) {
+        window.location.href = "../vendedor/vendedor.html"
+    }
+}
